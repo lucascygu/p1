a3f437b9f8e1cb0d9360b62a3efb6dab3e6843c7
Lucas Changyao Gu <113392627+lucascygu@users.noreply.github.com>
2024-07-23 13:02:38 -0700

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,6 +1,25 @@
 def read_calls(file: open) -> {(str, str): int}:
-    pass
+    calls = {}
 
+    for line in file:
+        parts = line.strip().split(':')
+        caller = parts[0]
+        callees = parts[1:]
+
+        for callee in callees:
+            if (caller, callee) in calls:
+                calls[(caller, callee)] += 1
+            else:
+                calls[(caller, callee)] = 1
+
+    return calls
 
 def call1to2(calls: {(str, str): int}) -> {str: {str: int}}:
-    pass
+    calls_nested_dict = {}
+
+    for (caller, callee), count in calls.items():
+        if caller not in calls_nested_dict:
+            calls_nested_dict[caller] = {}
+        calls_nested_dict[caller][callee] = count
+
+    return calls_nested_dict
