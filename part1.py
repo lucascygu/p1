a7935d2c60ec688e1e543ac4085f9c5c84971a0a
Lucas Changyao Gu <113392627+lucascygu@users.noreply.github.com>
2024-07-23 18:10:36 -0700

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,13 +1,16 @@
-def read_calls(file: open) -> {(str, str): int}:
+from typing import Dict, Tuple
+
+def read_calls(file: open) -> Dict[Tuple[str, str], int]:
     calls = {}
 
     for line in file:
         parts = line.strip().split(':')
+        if not parts:
+            continue
         caller = parts[0]
         callees = parts[1:]
 
         for callee in callees:
-            
             if (caller, callee) in calls:
                 calls[(caller, callee)] += 1
             else:
@@ -15,7 +18,7 @@ def read_calls(file: open) -> {(str, str): int}:
 
     return calls
 
-def call1to2(calls: {(str, str): int}) -> {str: {str: int}}:
+def call1to2(calls: Dict[Tuple[str, str], int]) -> Dict[str, Dict[str, int]]:
     calls_nested_dict = {}
 
     for (caller, callee), count in calls.items():
