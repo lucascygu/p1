ff6c9593100221556921ed22514925aa2a385ca5
Lucas Changyao Gu <113392627+lucascygu@users.noreply.github.com>
2024-07-24 09:15:35 -0700

    Update part4.py

--- part4.py
+++ part4.py
@@ -1,3 +1,28 @@
 def stable_stock_matching(buyers_preferences, stocks_preferences):
+    free_buyers = list(buyers_preferences.keys())
+    matches = {}
 
-    pass
+    proposals = {buyer: [] for buyer in buyers_preferences}
+
+    while free_buyers:
+        buyer = free_buyers.pop(0)
+        buyer_prefs = buyers_preferences[buyer]
+        
+        for stock in buyer_prefs:
+            if stock not in proposals[buyer]:
+                proposals[buyer].append(stock)
+                
+                if stock not in matches:
+                    matches[stock] = buyer
+                    break
+                else:
+                    current_buyer = matches[stock]
+                    stock_prefs = stocks_preferences[stock]
+                    
+                    if stock_prefs.index(buyer) < stock_prefs.index(current_buyer):
+                        matches[stock] = buyer
+                        free_buyers.append(current_buyer)
+                        break
+
+    result = {buyer: stock for stock, buyer in matches.items()}
+    return result
