c617a7e0b142fa377c67c8d41d144835682eefd4
Lucas Changyao Gu <113392627+lucascygu@users.noreply.github.com>
2024-07-24 11:23:53 -0700

    Update part6.py

--- part6.py
+++ part6.py
@@ -1,1 +1,34 @@
-#empty for now
+class PeekableIterator:
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self._has_next = None
+        self._next_item = None
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._has_next is None:
+            self._next_item = next(self.iterator)
+        self._has_next = None
+        return self._next_item
+
+    def peek(self):
+        if self._has_next is None:
+            try:
+                self._next_item = next(self.iterator)
+                self._has_next = True
+            except StopIteration:
+                self._has_next = False
+        if not self._has_next:
+            raise StopIteration
+        return self._next_item
+
+    def has_next(self):
+        if self._has_next is None:
+            try:
+                self._next_item = next(self.iterator)
+                self._has_next = True
+            except StopIteration:
+                self._has_next = False
+        return self._has_next
